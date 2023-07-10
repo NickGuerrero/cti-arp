b8919963264081991b0ea78f55dae89ab49817ac
Sergio Rojas <sergiorojasava@gmail.com>
2023-07-09 17:11:27 -0700

    cleanup time

--- src/time/isSameWeek.ts
+++ src/time/isSameWeek.ts
@@ -1,6 +1,6 @@
-var isSameWeek = function (dateA, dateB) {
+function isSameWeek(dateA, dateB) {
     var newDateA = new Date (dateA).setHours(0,0,0,0);
     var newDateB = new Date (dateB).setHours(0,0,0,0);
   
     return getWeek(new Date(newDateA)) === getWeek(new Date(newDateB));
-};
+}
